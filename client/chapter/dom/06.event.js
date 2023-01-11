737ca19c92367aa03933cd1fc3aa0abde58c21fd
realldk <realldk@naver.com>
2023-01-11 16:43:46 +0900

    버블링까지

---
+++ client/chapter/dom/06.event.js
@@ -0,0 +1,52 @@
+/* ---------------------------------------------------------------------- */
+/* Event Handling                                                         */
+/* ---------------------------------------------------------------------- */
+
+
+/* 이벤트 핸들링 3가지 방법 --------------------------------------------------- */
+
+// 1. HTML 속성 : onclick="handler()"
+// 2. DOM 프로퍼티 : element.onclick = handler
+// 3. 메서드 : element.addEventListener(event, handler[, phase])
+
+
+/* 이벤트 추가/제거 --------------------------------------------------------- */
+
+const first = getNode('.first');
+const ground = getNode('.ground');
+const ball = getNode('.ball');
+
+function handler(){
+  console.log('hit !');
+  css('.second', 'display', 'none');
+}
+
+first.addEventListener('click', handler);
+
+// 지우는 유틸함수? 여기 모르겠음
+// function bindEvent(node, type, handler){
+//   if(typeof node === 'string'){
+//     node = getNode(node);
+//   }
+//   if(!(/mouseenter|click|mousemove|mouseleave/g.test(type))){
+//     typeError('bindEvent 함수의 두 번째 인자는 유효한 이벤트 타입 이어야 합니다.');
+//   }
+//   node.addEventListener(type, handler);
+
+//   return ()=> node.removeEventListener(type, handler);
+// }
+
+// const off = bindEvent('.first', 'click', handler)
+// bindEvent('.second', 'click', off);
+
+// 공
+ground.addEventListener('click', function(e){
+  console.log(e.offsetX, e.offsetY);
+  ball.style.transform = `translate(${e.offsetX - ball.offsetWidth / 2}px, ${e.offsetY - ball.offsetHeight / 2}px)`;
+})
+
+ground.addEventListener('mousemove', function(e){
+  ball.style.transform = `translate(${e.offsetX - ball.offsetWidth / 2}px, ${e.offsetY - ball.offsetHeight / 2}px)`;
+})
+// - addEventListener
+// - removeEventListener
---
+++ client/chapter/dom/07.bubble.js
@@ -0,0 +1,43 @@
+/* ---------------------------------------------------------------------- */
+/* Event bubbling & capturing                                             */
+/* ---------------------------------------------------------------------- */
+
+
+/* 버블링 ----------------------------------------------------------------- */
+
+const visual = getNode('.visual');
+const news = getNode('.news');
+const desc = getNode('.desc');
+
+// visual.addEventListener('click', function(){
+//   console.log('%c visual','background:dodgerblue');
+// })
+// news.addEventListener('click', function(){
+//   console.log('%c news','background:orange');
+// })
+// // desc.addEventListener('click', function(){
+// //   console.log('%c desc','background:pink');
+// // })
+
+// //원하는 구역만 찍기 위해서, 안에 메서드
+// desc.addEventListener('click', function(e){
+//   e.stopPropagation()
+//   console.log('%c desc','background:pink');
+// })
+
+// e.target과 e.currentTarget
+visual.addEventListener('click', (e) => {
+  // console.log('target: ', e.target ); // 마우스가 제일 처음 클릭한 애
+  // console.log('currentTarget: ', e.currentTarget) // 
+  // console.log(this === e.currentTarget);
+  // console.log(e.currentTarget); //console.log(this) 는 window가 나온다.
+  console.log('%c visual','background:dodgerblue');
+  css('.pop', 'display', 'block');
+})
+
+getNode('.pop').addEventListener('click', (e)=>{
+  e.stopPropagation();
+  css('.pop', 'display', 'none');
+})
+
+/* 캡처링 ----------------------------------------------------------------- */
