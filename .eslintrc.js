d17c0496eb82dfb29b2593b2f65d276f83a4990e
realldk <realldk@naver.com>
2023-01-13 11:01:21 +0900

    함수형 계산기 더하기기능

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
-      eslint no-undef:'warn'
-      eslint no-unused-vars: 'off'
+
     }
 }
--- client/main.js
+++ client/main.js
@@ -3,18 +3,52 @@
 // 3. 이벤트 기본동작 차단하기
 // 4. 두 수의 합을 더해주기
 // 5. 화면에 출력하기
+// 이걸 다 함수형으로 만들기!
 
+eslint no-undef:'warn';
+eslint no-unused-vars: 'off';
+
+const firstInput = getNode('#firstNumber');
+const secondInput = getNode('#secondNumber');
 const done = getNode('#done');
 
+function getInputValue(node) {
+  if(typeof node === 'string') node = getNode(node);
+  if(node.tagName !== 'INPUT') refError('getInputValue 함수는 INPUT ELEMENT만 허용합니다.');
+  return node.value;
+}
+
+const sum = (valueA, valueB) => valueA + valueB;
+
+function clearContents(node) {
+  if(typeof node === 'string') node =getNode(node);
+  node.textContent = '';
+}
 
 function handler(e) {
   e.preventDefault();
   
-  let firstValue = getNode('#firstNumber');
-  let secondValue = getNode('#secondNumber');
+  let firstValue = +getInputValue(firstInput);
+  let secondValue = +getInputValue(secondInput);
+  let total = sum(firstValue, secondValue);
   let result = getNode('.result');
-  result.textContent = +(firstValue.value) + +(secondValue.value);
   
+  clearContents(result);
+  insertLast(result, total);
+}
+
+
+function inputHandler(e) {
+  let firstValue = +getInputValue(firstInput);
+  let secondValue = +getInputValue(secondInput);
+  let total = sum(firstValue, secondValue);
+  let result = getNode('.result');
+  
+  clearContents(result);
+  insertLast(result, total);
 }
 
 done.addEventListener('click', handler);
+
+firstInput.addEventListener('change', inputHandler);
+secondInput.addEventListener('change', inputHandler);
