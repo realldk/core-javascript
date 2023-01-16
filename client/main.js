fbe1e8aabef051115921d6305920b945e407acda
realldk <realldk@naver.com>
2023-01-16 16:45:57 +0900

    주접 클립보드 복사 복습

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+/* global gsap */
+
 import {getNode, getInputValue, getRandom, insertLast, clearContents, isNumericString, showAlert, clickCopy, addClass, removeClass} from "./lib/index.js"
 import { jujeobData } from "./data/data.js"
 const submit = getNode('#submit');
@@ -15,13 +17,13 @@ function clickSubmitHandler(e) {
     showAlert('.alert-error', '제대로 이름 입력');
 
     // 밑의 것 GSAP으로 해보기
-    gsap.fromTo(resultArea, 0.01, {x:-5}, {x:5, clearProps:"x", repeat:20})
+    // gsap.fromTo(resultArea, 0.01, {x:-5}, {x:5, clearProps:"x", repeat:20})
     // gsap.fromTo(타겟, duration, vars object from, vars object to)
     //shake 흔들어주는 기능 추가
     addClass(resultArea,'shake');
     //shake 기능 1초마다 없애주기
     setTimeout(() => {
-      removeClass(resultArea,'shake');
+      removeClass(resultArea,'shake')
     }, 1000);
     return;
   }
@@ -37,7 +39,7 @@ function clickSubmitHandler(e) {
 function clickCopyHandler() {
   let text = resultArea.textContent;
   //navigator.clipboard.writeText(text)
-  console.log(clickCopy(text))  // return값이 있는거랑 왜 promise가 뜨는건지
+  clickCopy(text)  // return값이 있는거랑 왜 promise가 뜨는건지
   // clickCopy(text).then(()=>{
   // showAlert('.alert-success','클립보드 복사가 완료됐습니다.', 2000);
   // })
